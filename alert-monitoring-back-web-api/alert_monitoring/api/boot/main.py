b67d3b7cdcb2bb5d333b089e47eca960236babfe
ccsantam ccsantam@mercadona.com <ccsantam@mercadona.es>
2026-04-30 10:01:44 +0200

    cambios

--- alert-monitoring-back-web-api/alert_monitoring/api/boot/main.py
+++ alert-monitoring-back-web-api/alert_monitoring/api/boot/main.py
@@ -8,9 +8,7 @@ from fwkpy_lib_fastapi import FastAPIBuilder
 from fwkpy_lib_core.common.injector import Injector
 from fwkpy_lib_utils.common.i18n.internationalization import load_translations, set_i18n
 from fwkpy_lib_database.synchronous.middlewares import add_session_middleware
-
 from fastapi.middleware.cors import CORSMiddleware
-
 
 set_i18n()
 translations_path = Path(os.path.dirname(__file__))
