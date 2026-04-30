b62133f6b51678662a8c392d1dc32709815b4975
ccsantam ccsantam@mercadona.com <ccsantam@mercadona.es>
2026-04-30 13:15:35 +0200

    se añaden tipos por defecto y si está sobreescrito al modelo de datos

--- alert-monitoring-back-web-api/alert_monitoring/api/domain/models/alert.py
+++ alert-monitoring-back-web-api/alert_monitoring/api/domain/models/alert.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-from typing import List, Optional
+from typing import List, Optional, Literal
 
 class Alert(BaseModel):
     name: str = Field(..., description="Nombre de la alerta")
@@ -12,7 +12,16 @@ class Alert(BaseModel):
     origin_link: Optional[str] = Field(None, description="Enlace directo a la definición")
 
     # Campos que pueden ser deducibles o inferidos
-    microservice: Optional[str] = Field(None, description="Solución/Aplicación a la que pertenece")
-    solution: Optional[str] = Field(None, description="Solución/Aplicación a la que pertenece")
+    microservice: Optional[str] = Field(None, description="Microservicio al que pertenece")
+    solution: Optional[str] = Field(None, description="PI frabricado")
     notification_channel: Optional[str] = Field(None, description="Canal o destino de notificación")
     confidence_level: float = Field(0.0, ge=0.0, le=1.0, description="Nivel de confianza del mapeo (0.0 a 1.0)")
+
+    alert_type: Literal["Por Defecto", "Ad-hoc"] = Field(
+        default="Ad-hoc", 
+        description="Indica si es un alertado por defecto ofrecido por Observabilidad o Ad-hoc solicitado por los equipos"
+    )
+    is_overridden: bool = Field(
+        default=False, 
+        description="True si esta alerta 'Por Defecto' ha sido deshabilitada/sustituida por una versión Ad-hoc (HU-22 / CU-04)"
+    )
